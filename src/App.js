411324289fa0a526ae367ef7052ed0be6404137e
Dave jan <devilvagrant@gmail.com>
2018-04-30 11:18:37 -0400

    To dos
    info and todos.
    Layout folder and js file added
    
    Co-Authored-By: Ankita Sharma <er.ankitasharma15@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import Layout from './Containers/Layout/Layout'
 
 
 class App extends Component {
     render() {
         
         return (
-            <div>
-                <h1>Test App</h1>
-            </div>
+            <Layout/>
         );
     }
 }
---
+++ src/Containers/Layout/Layout.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+class Layout extends Component {
+    state = {
+        
+    }
+    
+    render() {
+        
+        return (
+            <div>
+                <h1>Test App</h1>
+                <h3>Notes</h3>
+                <ul>
+                    <li>To do: Add packages: Apollo and redux*</li>
+                    <li>To do: in Containers: create Layout to set</li>
+                    <li>To do: Start integration with graphCMS</li>
+                    <li>Info: Layout to be parent structure </li>
+                </ul>
+                
+                <p>* if necessary</p>
+            </div>
+        );
+    }
+}
+
+export default Layout;
